39d0289f397f26a6f75c89a92719abbafbb3486c
sneiderV <e.velandia2164@uniandes.edu.co>
2018-03-19 15:23:19 -0500

    WebView

--- ControlWebView/app/src/main/java/com/example/sneider/webview/ActivityWeb.java
+++ ControlWebView/app/src/main/java/com/example/sneider/webview/ActivityWeb.java
@@ -2,12 +2,31 @@ package com.example.sneider.webview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class ActivityWeb extends AppCompatActivity {
+
+    //encapsular datos
+    WebView wv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web);
+
+        wv = (WebView)findViewById(R.id.wv);
+
+        //>>recuperamos la URL de la MainActivity
+        String url =  getIntent().getStringExtra("sitioWeb");
+        //control del navegador a usar DENTRO DE MI APP
+        wv.setWebViewClient(new WebViewClient());
+        wv.loadUrl("http://"+url);
+    }
+
+    public void cerrar (View vie){
+        //termina el activity en el que esta
+        finish();
     }
 }
--- ControlWebView/app/src/main/java/com/example/sneider/webview/MainActivity.java
+++ ControlWebView/app/src/main/java/com/example/sneider/webview/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.sneider.webview;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    //crear objeto de lo que estamos usando
+    private EditText et;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        et = (EditText)findViewById(R.id.txt_web);
     }
+
+    //met. navergar
+    public void navegar (View view){
+        Intent i = new Intent(this, ActivityWeb.class);
+        //enviamos datos de activity a activity
+        i.putExtra("sitioWeb",et.getText().toString());
+        startActivity(i);
+    }
+
 }
