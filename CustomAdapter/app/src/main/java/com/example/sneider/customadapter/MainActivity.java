c8cc1726c3859a89260e1e59ec488698e5568e0f
sneiderV <e.velandia2164@uniandes.edu.co>
2018-03-24 00:31:22 -0500

    Adapter
    creo un proyecto que usa una modificacion de un adapter, para mostrarlo en un listview

---
+++ CustomAdapter/app/src/main/java/com/example/sneider/customadapter/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.sneider.customadapter;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ArrayList image_details = getListData();
+        final ListView lv1 = (ListView) findViewById(R.id.custom_list);
+        lv1.setAdapter(new CustomListAdapter(this, image_details));
+        lv1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> a, View v, int position, long id) {
+                Object o = lv1.getItemAtPosition(position);
+                NewsItem newsData = (NewsItem) o;
+                Toast.makeText(MainActivity.this, "Selected :" + " " + newsData, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private ArrayList getListData() {
+        ArrayList<NewsItem> results = new ArrayList<NewsItem>();
+        NewsItem newsData = new NewsItem();
+        newsData.setHeadline("Dance of Democracy");
+        newsData.setReporterName("Pankaj Gupta");
+        newsData.setDate("May 26, 2013, 13:35");
+        results.add(newsData);
+
+        // Add some more dummy data for testing
+        return results;
+    }
+}
--- TabHost/app/src/main/java/com/example/sneider/tabhost/MainActivity.java
+++ TabHost/app/src/main/java/com/example/sneider/tabhost/MainActivity.java
@@ -2,12 +2,41 @@ package com.example.sneider.tabhost;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TabHost;
 
 public class MainActivity extends AppCompatActivity {
+
+    TabHost th;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        th = (TabHost)findViewById(R.id.tabHost);
+
+        //Tab 1
+        th.setup();
+        TabHost.TabSpec ts1=th.newTabSpec("Tab1");
+        ts1.setIndicator("Bienvenido");
+        ts1.setContent(R.id.tab1);
+        th.addTab(ts1);
+
+        //Tab 2
+        th.setup();
+        TabHost.TabSpec ts2=th.newTabSpec("Tab2");
+        ts1.setIndicator("Contenido");
+        ts1.setContent(R.id.tab2);
+        th.addTab(ts2);
+
+        //Tab 3
+        th.setup();
+        TabHost.TabSpec ts3=th.newTabSpec("Tab3");
+        ts1.setIndicator("Despedida");
+        ts1.setContent(R.id.tab3);
+        th.addTab(ts3);
+
     }
+
+
 }
